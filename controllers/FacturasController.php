2056647f576f77f81135fbd511dc01eb098161ac
Raubel <raubel1993@gmail.com>
2022-05-25 18:49:37 -0400

    Procesar Factura

--- controllers/FacturasController.php
+++ controllers/FacturasController.php
@@ -10,8 +10,27 @@ use app\models\Factura;
 
 class FacturasController extends Controller
 {
+    public function actions()
+    {
+        $actions = parent::actions();
+        $actions['options'] = [
+            'class' => 'yii\rest\OptionsAction',
+        ];
+        return $actions;
+    }
     public function actionIndex()
     {
         return Factura::search(Yii::$app->request->queryParams);
+    }
+    public function actionProcesada($id)
+    {
+        $factura = Factura::findOne(['id_doc' =>$id]);
+        if ($factura) {
+            $factura->estado = 1;
+            $factura->save();
+            return $factura;
+        } else {
+            throw new \yii\web\NotFoundHttpException("Factura no encontrada");
+        }
     }
 }
